7c3ebbee490c122e16d98a1aa604a2dbb65cdef7
adamay-design <139445800+adamay-design@users.noreply.github.com>
2023-12-06 17:18:25 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -12,13 +12,39 @@ jQuery(function($){
 
 window.onload = function() {
 
+  $(function(){
+    var $btn = $('.gallery-btn-container [data-filter]'),
+        $list = $('.gallery [data-category]');
+     
+    $btn.on('click', function(e) {
+      e.preventDefault();
+         
+      var $btnTxt = $(this).attr('data-filter');
+         
+      $btn.removeClass('active'); // すべてのボタンから active クラスを削除
+      $(this).addClass('active'); // クリックされたボタンに active クラスを追加
+         
+      if ($btnTxt == 'all'){
+        $list.fadeOut().promise().done(function() {
+          $list.addClass('animate').fadeIn();
+        });
+      } else {
+        $list.fadeOut().promise().done(function() {
+          $list.filter('[data-category = "' + $btnTxt + '"]').addClass('animate').fadeIn();
+        });
+      }
+    });
+  });
+  
+  
 
   // lightbox オプションの設定※https://lokeshdhakar.com/projects/lightbox2/#options参照
 
   lightbox.option({
     'wrapAround': true,//グループ最後の写真の矢印をクリックしたらグループ最初の写真に戻る
     'albumLabel': ' %1 / total %2 ',//合計枚数中現在何枚目かというキャプションの見せ方を変更できる
-    'showImageNumberLabel': false  //false の場合、現在の画像番号とセット内の画像の総数を示すテキスト (例: "image 2 of 4") が非表示にできる
+    'showImageNumberLabel': false,  //false の場合、現在の画像番号とセット内の画像の総数を示すテキスト (例: "image 2 of 4") が非表示にできる
+    'alwaysShowNavOnTouchDevices':  true //true時、スマホ閲覧で左右の矢印を常に表示
   });
 
 
@@ -33,7 +59,7 @@ window.onload = function() {
   }else{
 
     var map = L.map('map');
-    map.setView([33.619837, 130.367861], 12.1);
+    map.setView([33.591796023990646, 130.40053679829194], 14);
 
     L.tileLayer(
         'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -44,6 +70,21 @@ window.onload = function() {
 
     L.marker([33.670655, 130.444721],{title:"九州産業大学 2号館"}).addTo(map)
     .bindPopup('<p>九州産業大学 2号館</p>')
+    .openPopup();
+    L.marker([33.591796023990646, 130.40053679829194],{title:"天神ビジネスセンター"}).addTo(map)
+    .bindPopup('<p>天神ビジネスセンター</p>')
+    .openPopup();
+    L.marker([33.59197591938904, 130.4032882343487],{title:"アクロス福岡"}).addTo(map)
+    .bindPopup('<p>アクロス福岡</p>')
+    .openPopup();
+    L.marker([33.59254288492705, 130.40356137517006],{title:"AQUA HAKATA"}).addTo(map)
+    .bindPopup('<p>AQUA HAKATA</p>')
+    .openPopup();
+    L.marker([33.5880221289339, 130.39915365055128],{title:"ソラリアターミナルビル"}).addTo(map)
+    .bindPopup('<p>ソラリアターミナルビル</p>')
+    .openPopup();
+    L.marker([33.591575065574865, 130.3986365156948],{title:"福岡ダイヤモンドビル"}).addTo(map)
+    .bindPopup('<p>福岡ダイヤモンドビル</p>')
     .openPopup();
   }
 
